019e7a755d852e3dd8f11f69154c1c335b2604a8
Kaue Alves <kaue_alves00@yahoo.com>
2022-08-09 15:18:23 -0300

    feat/ concluindo o requisito 2

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,18 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints: number;
+  static instance = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling.instance += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;   
+  }
+
+  static createdRacesInstances():number { return this.instance; }
+}
